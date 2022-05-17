2129d6331cc77d0ffd93486a76134d766f0a694b
Akumaw <dev@akumaw.com>
2022-05-17 18:20:47 +0200

    code review

--- getData.js
+++ getData.js
@@ -4,10 +4,17 @@ const jouerButton = document.querySelector('button')
 
 console.log(jouerButton);
 
+document.addEventListener("DOMContentLoaded", (event) => {
 
-jouerButton.addEventListener('click', event => {
-
-    jouerButton.innerHTML = `Nombre de clics : ${event.detail}`;
     
 
-})
+    jouerButton.addEventListener('click', event => {
+
+        jouerButton.innerHTML = `Nombre de clics : ${event.detail}`;
+        
+    
+    })
+
+});
+
+
